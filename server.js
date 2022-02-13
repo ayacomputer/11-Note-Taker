91391d14e33dab677e25df421500cad51de302bd
Ayako Woollan <ayako.woollan@gmail.com>
2022-02-13 17:30:22 +1100

    app working

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/api/notes', (req, res) =>
+app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
 );
 
